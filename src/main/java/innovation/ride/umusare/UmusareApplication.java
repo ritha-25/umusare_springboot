22b5609d0d9fa51357e25f98efde0b201b665618
nzaramba <rithanzaramba@gmail.com>
2026-09-19 01:41:51 +0000

    Umusare spingboot project structure

---
+++ src/main/java/innovation/ride/umusare/UmusareApplication.java
@@ -0,0 +1,13 @@
+package innovation.ride.umusare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmusareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmusareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/innovation/ride/umusare/UmusareApplicationTests.java
@@ -0,0 +1,13 @@
+package innovation.ride.umusare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmusareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
